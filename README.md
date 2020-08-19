009985ff288793229d83ecf6592a18d66d41372d
AMohammedAryan <69350797+AMohammedAryan@users.noreply.github.com>
2020-08-19 16:53:11 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Repository-Name
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,40 @@
+var bullet, wall;
+var speed, weight;
+var damage;
+var thickness;
+
+function setup() {
+  createCanvas(1600,400);
+
+  speed = random(223, 321);
+  weight = random(30, 52);
+  thickness = random(22, 83);
+
+  bullet = createSprite(50, 200, 60, 20);
+  bullet.shapeColor = "white";
+
+  wall = createSprite(1200, 200, thickness, 800);
+  wall.shapeColor = (80, 80, 80);
+
+  bullet.velocityX = speed;
+
+}
+
+function draw() {
+  background("black");
+
+
+  if(wall.x - bullet.x < bullet.width/2 + bullet.width/2){
+    damage = 0.5 * weight * speed * speed / thickness * thickness * thickness
+    bullet.velocityX = 0;
+
+    if(damage < 10){
+      bullet.shapeColor = "green";
+    }
+    if(damage > 10){
+      bullet.shapeColor = "red";
+    }
+  }
+  
+  drawSprites();
+}
